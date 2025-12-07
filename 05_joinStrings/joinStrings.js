6de18dce9b78b68d739b8593c8c75b9682c64232
TheGreatOverWolf <96438404+TheGreatOverWolf@users.noreply.github.com>
2025-12-07 08:40:23 +0100

    05

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -1,3 +1,13 @@
+const firstName = "Carlos" ;
+const lastName = "Stevenson" ;
+const birthYear = 1947 ;
+const thisYear = 1965 ;
+const fullName = firstName + " " + lastName ;
+const age = thisYear - birthYear
+const greeting = "Hello! My name is "+ fullName +" and I am "+age+" years old."
+
+
+
 /*
  First name: Carlos
  Last name: Stevenson
