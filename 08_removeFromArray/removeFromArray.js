95dac4c928a0755ee73785f88d9b6cc5a714d06d
TheGreatOverWolf <96438404+TheGreatOverWolf@users.noreply.github.com>
2025-12-09 09:33:29 +0100

    08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+    return arr.filter(element => !itemsToRemove.includes(element));
 };
 
 // Do not edit below this line
